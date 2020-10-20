dd55addc34cf07f8bd32c2101b2dca1e536b987b
akberovr <a.rovshan@code.edu.az>
2020-10-20 17:06:32 +0200

    initialize spring boot project

---
+++ src/test/java/com/bme/lab/ptl/PtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bme.lab.ptl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/bme/lab/ptl/integration/IntegrationTest.java
@@ -0,0 +1,34 @@
+package com.bme.lab.ptl.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author akberovr (Rovshan Akbarov)
+ * created on 2020-10-20
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegrationTest {
+
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getRoute_returnsAllRoutes()  throws Exception{
+        //arrange
+
+        //act
+        
+        //assert
+
+    }
+
+}
