34ab0ec865564db573b355379b5b61be70f17c65
imre-gabor <gabor@aut.bme.hu>
2020-11-26 17:48:13 +0100

    use junit 5 instead of junit 4

--- src/test/java/com/bme/lab/ptl/rest/RouteControllerTest.java
+++ src/test/java/com/bme/lab/ptl/rest/RouteControllerTest.java
@@ -1,34 +1,29 @@
 package com.bme.lab.ptl.rest;
 
-import com.bme.lab.ptl.domain.Route;
-import com.bme.lab.ptl.service.RouteService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
-
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.bme.lab.ptl.domain.Route;
+import com.bme.lab.ptl.service.RouteService;
 
 /**
  * @author akberovr (Rovshan Akbarov)
  * created on 2020-10-21
  */
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(RouteController.class)
 public class RouteControllerTest {
 
