47fb53cfe419ac3aeac62cb21e1d0fe0fdf237ea
akberovr <a.rovshan@code.edu.az>
2020-11-30 10:09:06 +0100

    add changed files before doing pull for new branch

--- src/test/java/com/bme/lab/ptl/integration/IntegrationTest.java
+++ src/test/java/com/bme/lab/ptl/integration/IntegrationTest.java
@@ -1,12 +1,17 @@
 package com.bme.lab.ptl.integration;
 
+import com.bme.lab.ptl.domain.Route;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author akberovr (Rovshan Akbarov)
@@ -14,7 +19,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
 
 
@@ -26,8 +31,12 @@ public class IntegrationTest {
         //arrange
 
         //act
-        ResponseEntity<Route> response;
+        ResponseEntity<Route> response =
+                restTemplate.getForEntity("/routes",Route.class);
         //assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getOrigin()).isEqualTo("Baku");
+        assertThat(response.getBody().getDestination()).isEqualTo("Budapest");
 
     }
 
